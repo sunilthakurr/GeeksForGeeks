cf1f2de6c2ad9ffc9c606ce5f1c7be617af950bc
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2023-01-01 18:02:36 +0530

    Create Count-Even-Length.java

---
+++ Count-Even-Length/Count-Even-Length.java
@@ -0,0 +1,45 @@
+class Solution
+{
+    public static final long mod = 1000000007;
+    public int  compute_value(int n)
+    {
+        // code here
+        int fact_n=1;
+        for(int i=1;i<=n;i++) fact_n = multi(fact_n,i);
+        
+        int fact_nr=fact_n;
+        int fact_r=1;
+        int ans=0;
+        for(int r=0;r<=n;r++) {
+            int denominator = multi(fact_nr,fact_r);
+            int val = divide(fact_n,denominator);
+            ans = add(ans,multi(val,val));
+            
+            fact_r = multi(fact_r,r+1);
+            if(n!=r) fact_nr = divide(fact_nr,n-r);
+        }
+        
+        return ans;
+    }
+    
+    public static long power(long a,long b) {
+        if(b==0) return 1;
+        long p = power(a,b/2);
+        p = multi(p,p);
+        if(b%2==1) p = multi(p,a);
+        return p;
+    }
+    
+    public static int add(long a,long b) {
+        return (int)(((a%mod)+(b%mod))%mod);
+    }
+    
+    public static int multi(long a,long b) {
+        return (int)(((a%mod)*(b%mod))%mod);
+    }
+    
+    // {1/b}%mod => {b^(-1)}%mod => power(b,mod-2);
+    public static int divide(long a,long b) {
+        return multi(a,power(b,mod-2));
+    }
+}
